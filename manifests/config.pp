ca834c681b59fc5b93f049c155f713ea22a623e4
Kevin Häfeli <kevin@helio.exchange>
2019-06-18 11:41:35 +0200

    :sparkles: Manage td-agent-bit config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,25 @@
-# @summary A short summary of the purpose of this class
+# @summary configures the main fluentbit main config
 #
-# A description of what this class does
+# Creates a puppet resource for every service, input, ouptut
+# Doesn't support filters (yet)
+# Includes all [service], [input] and [output] configs. (@include)
+# Sets global variables (@set)
 #
-# @example
+# @private
 #   include fluentbit::config
-class fluentbit::config {
+class fluentbit::config(
+  Optional[String]  $configfile = '/etc/td-agent-bit/td-agent-bit.conf',
+  ) {
+  assert_private()
+
+  # create configfile
+  file { $configfile:
+    ensure  => file,
+    mode    => '0644',
+    content => template('td-agent-bit.conf.erb'),
+    notify  => Service['td-agent-bit'],
+  }
+  # create service resource
+  # create input resource
+  # create output resource
 }
